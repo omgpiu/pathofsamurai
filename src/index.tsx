18d39499b336921e515655c6ed448be7e3285028
omgpiu <dyba@list.ru>
2020-06-25 12:54:01 +0300

    new

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 
 import * as serviceWorker from './serviceWorker';
-import state from './Rdux/State';
-import {renderTree} from './Render';
+import state, {RootStateType, subscribe} from './Rdux/State';
+
+import ReactDOM from "react-dom";
+import App from "./App";
+
+
+
+
+ const renderTree = (state: RootStateType) => {
+
+    ReactDOM.render(<App/>, document.getElementById('root'));
+};
+
 
 
 renderTree(state);
 
+
+subscribe(renderTree);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
