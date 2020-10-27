1645e536f5bf3c9191662cffa7e54d6fb6484e92
omgpiu <dyba@List.ru>
2020-10-27 19:39:42 +0300

    lesson 71 bind, change localState

--- src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatus.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatus.tsx
@@ -3,8 +3,21 @@ import React from 'react';
 
 class ProfileStatus extends React.Component<any, any> {
     state = {
-        editMode: false
+        editMode: false,
+        title: 'Status'
     };
+
+    activateEditMode() {
+        this.setState({
+            editMode: true
+        });
+    }
+
+    deactivateEditMode() {
+        this.setState({
+            editMode: false
+        });
+    }
 
 
     render() {
@@ -12,8 +25,9 @@ class ProfileStatus extends React.Component<any, any> {
 
 
             <div>
-                {!this.state.editMode ? <div><span>{this.props.status}</span></div>
-                    : <div><input value={this.props.status}/></div>}
+                {!this.state.editMode ?
+                    <div><span onDoubleClick={this.activateEditMode.bind(this)}>{this.props.status}</span></div>
+                    : <div><input  autoFocus={true} onBlur={this.deactivateEditMode.bind(this)} value={this.props.status}/></div>}
             </div>
         )
             ;
